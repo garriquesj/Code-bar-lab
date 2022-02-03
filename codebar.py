d652f4bb02d290eefd868ed8303448f5fa147f79
Jason <jay.garriques@gmail.com>
2022-02-03 14:51:06 -0500

    perfoming tests

--- codebar.py
+++ codebar.py
@@ -1,37 +1,39 @@
 
 class Member:
-    def __init__(self,full_name, introduce):
+    def __init__(self,full_name):
         self.full_name = full_name
-        self.introduce = introduce
     def welcome(self):
         print(f"Hello {self.full_name}!")
-jason = Member("jay","Whatsup")
+jason = Member("jay")
 jason.welcome()
 
-class Student:
-    def __init__(self,full_name, reason ):
+class Student(Member):
+    def __init__(self, full_name, reason ):
         super().__init__(full_name)
         self.reason= reason
-        print(f'{full_name} codes because ')
-jason = Students("jay", "I like to learn")
+    def about_me(self):
+        print(f"{self.full_name} codes because {self.reason}")
 
-class Instructor:
+stephen = Student("Stephen", "he like to learn")
+stephen.about_me()
+class Instructor(Member):
     def __init__(self, full_name, bio, skills):
         super().__init__(full_name)
         self.bio = bio
-        self.skills = []
-
-    def add_skill(self,new_skills):
+        self.skills = ["Python", "Javascript", "C++"]
+    def add_skill(self, new_skills):
         self.skills.extend(new_skills)
     def test(self):
-        print(f'My name is {full_name} I like {bio} my skills are{skills}')    
-eric = Instructor("Eric", "Duke and puppies",["coding","teaching","holding puppies"])
+        print(f"My name is {self.full_name} I like {self.bio} my skills are{self.skills}") 
+
+eric = Instructor("Eric", "Duke and puppies", "")
+eric.test()
 class Workshops:
     def __init__(self, date, subject, instuctors, students):
-        self.date=date
-        self.subject=subject
-        self.instructor=instructor
-        self.students=students
+        self.date = date
+        self.subject = subject
+        self.instructor = instructor
+        self.students = students
 
 
 
