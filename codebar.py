9d6171fe96f0ab934a415f9ddfec29f845f69c2c
Jason <jay.garriques@gmail.com>
2022-02-03 13:20:10 -0500

    testing

--- codebar.py
+++ codebar.py
@@ -1,33 +1,41 @@
 
 class Member:
     def __init__(self,full_name, introduce):
-        self.full_name=full_name#SELF LINKS PARAM TO KEY
-        self.introduce=introduce
-# ______________________________________________________
+        self.full_name = full_name
+        self.introduce = introduce
+    def welcome(self):
+        print(f"Hello {self.full_name}!")
+jason = Member("jay","Whatsup")
+jason.welcome()
+
 class Student:
     def __init__(self,full_name, reason ):
-        super().__init__(full_name)#calls full name in MEMBER CLASS
+        super().__init__(full_name)
         self.reason= reason
-# ______________________________________________________
+        print(f'{full_name} codes because ')
+jason = Students("jay", "I like to learn")
+
 class Instructor:
     def __init__(self, full_name, bio, skills):
-    #maybe I should tell it the parameter is a list
         super().__init__(full_name)
-        self.bio=bio
-        self.skills=[]#incase of multiple skills?
-# ______________________________________________________
-    def add_skill(self,new_skills):#take arguement of skills and self?do put this in or above instructor
-        self.skills.extend(new_skills)#NB extend method adds all the elements of an iterable (list, tuple, string etc.) to the end of the list.nope got match its arguements
-# ______________________________________________________
+        self.bio = bio
+        self.skills = []
+
+    def add_skill(self,new_skills):
+        self.skills.extend(new_skills)
+    def test(self):
+        print(f'My name is {full_name} I like {bio} my skills are{skills}')    
+eric = Instructor("Eric", "Duke and puppies",["coding","teaching","holding puppies"])
 class Workshops:
     def __init__(self, date, subject, instuctors, students):
         self.date=date
         self.subject=subject
         self.instructor=instructor
         self.students=students
-    # def add_participant(self.member,instructor,students):
 
 
 
-Instructor('eric')
+
+
+
 # ______________________________________________________
